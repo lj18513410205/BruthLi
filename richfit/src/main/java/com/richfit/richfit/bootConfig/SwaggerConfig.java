38a6632e14a65bf9e427e42c03371a5ef518718d
lj18513410205 <hlr2261225>
2019-11-10 21:58:46 +0800

    pom

--- richfit/src/main/java/com/richfit/richfit/bootConfig/SwaggerConfig.java
+++ richfit/src/main/java/com/richfit/richfit/bootConfig/SwaggerConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -21,5 +23,10 @@ public class SwaggerConfig {
     @Bean
     public Docket swaggerSpringMvcPlugin() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
+    }
+
+    @Bean
+    public SecurityConfiguration securityInfo() {
+        return new SecurityConfiguration(null, null, null, null, null, null, null, "");
     }
 }
--- richfit/src/main/java/com/richfit/richfit/service/SysUserServiceImpl.java
+++ richfit/src/main/java/com/richfit/richfit/service/SysUserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.richfit.richfit.service;
 
 
+
+
+import com.richfit.richfit.Dto.SysUserDto;
 import com.richfit.richfit.dao.SysUserDao;
 import com.richfit.richfit.model.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import com.alibaba.fastjson.JSON;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -35,35 +40,44 @@ public class SysUserServiceImpl implements SysUserService{
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-   /* @Override
-    public List<SysUser> listSys() {
-        List<SysUser> list = new ArrayList<>(3);
-        String sql = "select u.* from LJ u";
-        //这种只支持 String.class   Integer.class 不支持SysUser.class;
-       // List<SysUser> select_u_from_sysUser_u_ = jdbcTemplate.queryForList("select u.* from LJ u ", SysUser.class);
-        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
-        System.out.println(maps);
-        maps.stream().forEach(a->{
-            SysUser sys = new SysUser();
-            sys.setUserId((String) a.get("USERID"));
-            sys.setUserName((String) a.get("USERNAME"));
-            sys.setSex((String) a.get("SEX"));
-            list.add(sys);
-        });
-        return list;
-    }
-*/
 
+    /**
+     * 从List<A> copy到List<B>
+     * @param list
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    private static <T> List<T> copy(List<?> list,Class<T> clazz){
+        String oldOb = JSON.toJSONString(list);
+        return JSON.parseArray(oldOb, clazz);
+    }
 
 
     @Override
+    public List<SysUser> listSys() {
+        //List<SysUser> list = new ArrayList<>(3);
+        String sql = "select u.* from LJ u";
+        //这种只支持 String.class   Integer.class 不支持SysUser.class;
+        //List<SysUser> select_u_from_sysUser_u_ = jdbcTemplate.queryForList("select u.* from LJ u ", SysUser.class);
+        //下面这种没问题
+        //List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+        List<SysUserDto> query = jdbcTemplate.query(sql, new BeanPropertyRowMapper(SysUserDto.class));
+        List<SysUser> copy = SysUserServiceImpl.copy(query, SysUser.class);
+        return copy;
+    }
+
+
+
+
+    /*@Override
     public List<SysUser> listSys() {
         Query select_u_from_sysUser_u = entityManager.createQuery("select u from SysUser u");
         List<SysUser> resultList = select_u_from_sysUser_u.getResultList();
         System.out.println(resultList);
         return resultList;
     }
-
+*/
     @Override
     public void deleteListSys(String id) {
       //  Query query = entityManager.createQuery("delete from SysUser s where s.userId = :id ");
